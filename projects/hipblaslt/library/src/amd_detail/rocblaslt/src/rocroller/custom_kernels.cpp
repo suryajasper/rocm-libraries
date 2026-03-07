03cab735a07c8d031420e987c44a6a7e10926d02
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2026-03-07 00:09:44 +0000

    Added 256x192x256 wave

--- projects/hipblaslt/library/src/amd_detail/rocblaslt/src/rocroller/custom_kernels.cpp
+++ projects/hipblaslt/library/src/amd_detail/rocblaslt/src/rocroller/custom_kernels.cpp
@@ -465,6 +465,15 @@ void preloadCustomKernels(SolutionCache& cache)
                                                    {128, 2, 1},
                                                    getCoPath() / "rr_custom_kernels.co"));
 
+            params.workgroupTile = {256, 192, 256};
+            cache.addKernel(mxfp4Kernel,
+                            params,
+                            createCustomGemmKernel("wave_mxfp4_dynamic_gemm_256x192x256",
+                                                   mxfp4Kernel,
+                                                   params.workgroupTile,
+                                                   {64, 4, 1},
+                                                   getCoPath() / "rr_custom_kernels.co"));
+
             params.workgroupTile = {256, 224, 256};
             cache.addKernel(mxfp4Kernel,
                             params,
